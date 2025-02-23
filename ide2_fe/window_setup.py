14f2d2b69bfa37856b7cb608d1423d7f0f17b318
nathanhu26 <hu853@purdue.edu>
2025-02-22 23:58:34 -0500

    Import and save buttons

--- ide2_fe/window_setup.py
+++ ide2_fe/window_setup.py
@@ -1,9 +1,7 @@
 from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtWidgets import *
 
-def import_button_clicked():
-        print("nokotan") 
-
+#Heavy lifting happens here
 class MainIdeWindow(QMainWindow):   
 
     def __init__(self):
@@ -16,26 +14,27 @@ class MainIdeWindow(QMainWindow):
 
         # Defining the base component widgets for our layout
         file_list = QTextEdit()
-        first_file = QTextEdit()
+        self.editor = QTextEdit()
 
         edit_window = QTabWidget()
         term_window = QTabWidget()
 
         import_button = QPushButton()
         import_button.setText("Import File or Folder")
-        import_button.clicked.connect(import_button_clicked)
+        import_button.clicked.connect(self.import_button_clicked)
 
 
-        settings_button = QPushButton()
-        settings_button.setText("Settings") #To be implemented later...
+        save_button = QPushButton()
+        save_button.setText("Save") 
+        save_button.clicked.connect(self.save_button_clicked)
 
         # Tabulation for files
-        edit_window.addTab(first_file, "First Tab")
+        edit_window.addTab(self.editor, "First Tab") #class attribute 
 
-        #Horizontal splitter for buttons (import, settings, etc.)
+        #Horizontal splitter for buttons (import, save, etc.)
         buttons_split = QSplitter(Qt.Orientation.Horizontal)
         buttons_split.addWidget(import_button)
-        buttons_split.addWidget(settings_button)
+        buttons_split.addWidget(save_button)
 
         #Add vertical splitter for left side (buttons + file directories)
         left_split = QSplitter(Qt.Orientation.Vertical)
@@ -62,3 +61,19 @@ class MainIdeWindow(QMainWindow):
 
         # Set the central widget of the Window.
         self.setCentralWidget(central_widget)
+    
+    def import_button_clicked(self):
+        print("Trigger import")
+        file_name, _ = QFileDialog.getOpenFileName(self, "Open File", "", "Python Files (*.py);;All Files (*)")
+        if file_name:
+            with open(file_name, "r", encoding="utf-8") as file:
+                self.editor.setText(file.read())
+
+    def save_button_clicked(self):
+        print("Trigger save")
+        file_name, _ = QFileDialog.getSaveFileName(self, "Save File", "", "Python Files (*.py);;All Files (*)")
+        if file_name:
+            with open(file_name, "w", encoding="utf-8") as file:
+                #print("Saving...")
+                #print(self.editor.toPlainText())
+                file.write(self.editor.toPlainText())
